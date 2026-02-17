cf4d6b7593c75a3dfc1d0f5c46cf2dd6900e41b5
leandroincan <leandroincan@gmail.com>
2026-02-16 22:46:53 -0800

    Update budget_app.py

--- budget_app.py
+++ budget_app.py
@@ -19,7 +19,7 @@ with st.form("add_expense", clear_on_submit=True):
             notion.pages.create(
                 parent={"database_id": DATABASE_ID},
                 properties={
-                    "Name": {"title": [{"text": {"content": name}}]},
+                    "Item": {"title": [{"text": {"content": name}}]},
                     "Cost": {"number": cost},
                     "Who": {"select": {"name": who}},
                     "Archived": {"checkbox": False}
